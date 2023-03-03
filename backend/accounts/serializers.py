6a2fdb21893c9919a478b6aea71a256a41bb3262
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-03-03 20:25:15 +0330

    Hash password during user create method

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 
 from .models import User
 
@@ -7,3 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'password', 'phone', 'address', 'gender', 'age', 'description', 'first_name', 'last_name', 'email']
+        
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super(UserSerializer, self).create(validated_data)
